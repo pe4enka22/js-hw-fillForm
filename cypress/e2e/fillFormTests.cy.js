a769ac2ef1084443fd19ff3f6fd944e7fc6325f4
pe4enka22 <missis.bug@gmail.com>
2024-01-30 21:25:06 +0200

    added param tests

--- cypress/e2e/fillFormTests.cy.js
+++ cypress/e2e/fillFormTests.cy.js
@@ -1,53 +1,85 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/')
+let formData = [{
+    //inlineform
+    fieldSelector: '[placeholder="Jane Doe"]',
+    inputValue: "Inline Name",
+    getValue: "Inline Name"
+},{
+    fieldSelector: 'form.form-inline [placeholder="Email"]',
+    inputValue: "inline@email.test",
+    getValue: "inline@email.test"
+}, {
+    //using the grid
+    fieldSelector: '#inputEmail1',
+    inputValue: "grid@email.test",
+    getValue: "grid@email.test"
+},{
+    fieldSelector: '#inputPassword2',
+    inputValue: "gridPassWord1",
+    getValue: "gridPassWord1"
+},{
+    //Form without labels
+    fieldSelector: '[placeholder="Recipients"]',
+    inputValue: "LabelRecipient",
+    getValue: "LabelRecipient"
+},{
+    fieldSelector: '[placeholder="Subject"]',
+    inputValue: "Label Subject",
+    getValue: "Label Subject"
+}, {
+    fieldSelector: '[placeholder="Message"]',
+    inputValue: "Label Message",
+    getValue: "Label Message"
+},{
+    //Horizontal form fill
+    fieldSelector: '#inputEmail3',
+    inputValue: "horizontal@email.test",
+    getValue: "horizontal@email.test"
+},{
+    fieldSelector: '#inputPassword3',
+    inputValue: "horizontalPassWord1",
+    getValue: "horizontalPassWord1"
+},{
+    //Basic form fill
+    fieldSelector: '#exampleInputEmail1',
+    inputValue: "basic@email.test",
+    getValue: "basic@email.test"
 
-    cy.log('Open Dark Theme');
-    cy.get('[alt="Material Dark Theme"]').click();
+},{
+    fieldSelector: '#exampleInputPassword1',
+    inputValue: "inputPassWord1",
+    getValue: "inputPassWord1"
+},{
+    //Block form
+    fieldSelector: '#inputFirstName',
+    inputValue: "Block FirstName",
+    getValue: "Block FirstName"
+},{
+    fieldSelector: '#inputLastName',
+    inputValue: "Block LastName",
+    getValue: "Block LastName"
+},{
+    fieldSelector: '#inputEmail',
+    inputValue: "block@email.test",
+    getValue: "block@email.test"
+},{
+    fieldSelector: '#inputWebsite',
+    inputValue: "block.com",
+    getValue: "block.com"
+}]
 
-    cy.log('Open Layouts page');
-    cy.get('[title="Forms"]').click();
-    cy.get('[href="/pages/forms/layouts"]').click();
+     formData.forEach(formData => {
+         it(`Fill in the ${formData.fieldSelector} field`, () => {
 
-    cy.log('Inline form locators');
-    cy.get('[placeholder="Jane Doe"]');
-    cy.get('form.form-inline [placeholder="Email"]');
-    cy.get('form.form-inline .custom-checkbox');
-    cy.get('form.form-inline button');
+             cy.log('Open layout page');
+             cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+             cy.log('Open Dark Theme');
+             cy.get('[alt="Material Dark Theme"]').click();
+             cy.log('Open Layouts page');
+             cy.get('[title="Forms"]').click();
+             cy.get('[href="/pages/forms/layouts"]').click();
 
-    cy.log('Using the Grid locators');
-    cy.get('#inputEmail1');
-    cy.get('#inputPassword2');
-    cy.get('form [type="radio"]').eq(0);
-    cy.get('form [type="radio"]').eq(1);
-    cy.get('form [type="radio"]').eq(2);
-    cy.get('nb-card-body .offset-sm-3.col-sm-9>button.status-primary');
+             cy.get(formData.fieldSelector).type(`${formData.inputValue}`);
+             cy.get(formData.fieldSelector).should('contain', formData.getValue);
 
-    cy.log('Form without labels locators');
-    cy.get('[placeholder="Recipients"]');
-    cy.get('[placeholder="Subject"]');
-    cy.get('[placeholder="Message"]');
-    cy.get('nb-card-body .form-group+button.status-success');
-
-    cy.log('Horizontal form locators');
-    cy.get('#inputEmail3');
-    cy.get('#inputPassword3');
-    cy.get('form.form-horizontal .checkbox');
-    cy.get('form.form-horizontal button');
-
-    cy.log('Basic form locators');
-    cy.get('#exampleInputEmail1');
-    cy.get('#exampleInputPassword1');
-    cy.get('button[status="danger"]');
-    cy.get('div .custom-checkbox').eq(1);
-
-
-    cy.log('Block form locators');
-    cy.get('#inputFirstName');
-    cy.get('#inputLastName');
-    cy.get('#inputEmail');
-    cy.get('#inputWebsite');
-    cy.get('nb-card-body > button');
-
-  })
-})
+         })
+     })
